b5a9f96a327b1d91593aa66476058ba86edf93e6
Sabiq Mohammed <sabiq.mohammed4@gmail.com>
2024-09-09 11:52:52 +0530

    structure

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,28 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 1,
+      'name': 'Apples',
+      'price': 10.5,
+      'description': 'Fresh and juicy apples.',
+      'imageUrl':
+          'https://www.telegraph.co.uk/content/dam/news/2023/05/23/TELEMMGLPICT000336809344_16848508364990_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpeg?imwidth=680',
+    },
+    {
+      'id': 2,
+      'name': 'Oranges',
+      'price': 6.5,
+      'description': 'Fresh and juicy oranges.',
+      'imageUrl':
+          'https://static.scientificamerican.com/sciam/cache/file/68A02F93-F3F9-40C1-8A157B9F77108A85_source.jpg?w=600',
+    },
+    {
+      'id': 3,
+      'name': 'Bananas',
+      'price': 5,
+      'description': 'Fresh and juicy bananas.',
+      'imageUrl':
+          'https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bunch-of-bananas-67e91d5.jpg?quality=90&resize=440,400',
+    }
+  ];
+}
